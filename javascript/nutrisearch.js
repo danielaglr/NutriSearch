6edda9a494699762b79f9a978e0a6bd19b58402f
Daniel <81238732+danielaglr@users.noreply.github.com>
2022-01-03 10:02:10 -0800

    Began work on actual API search function
    Created JavaScript file to house API search function and created basic search bar

---
+++ javascript/nutrisearch.js
@@ -0,0 +1,41 @@
+
+function createNode(element) {
+    return document.createElement(element);
+};
+
+function appendNode(parent, el) {
+    return parent.appendChild(el);
+};
+
+async function apiSearch() {
+
+    const edamamId = "e3d355ba";
+    const edamamKey = "67961828f045b54be7e5687dee059d6b";
+
+    var userInput = document.getElementById('recipe-find-search').value;
+
+    const edamamUrl = `https://api.edamam.com/search?q=${userInput}&app_id=${edamamId}&app_key=${edamamKey}`;
+
+    const ul = document.getElementsByClassName('recipe-display');
+
+    await fetch(edamamUrl).then(r => r.json()).then(function(data) {
+        let recipes = data.hits
+        return recipes.map(function(recipe) {
+            let li = createNode('li');
+            let img = createNode('img');
+            let title = createNode('h3');
+            let nutrp = createNode('p');
+            img.src = recipe.recipe.image;
+            title.innerHTML = `${recipe.recipe.label}`;
+            nutrp.innerHTML = 
+            `Calories: ${Math.round(recipe.recipe.totalNutrients.ENERC_KCAL.quantity)} ${recipe.recipe.totalNutrients.ENERC_KCAL.unit} 
+            <br> Protein: ${Math.round(recipe.recipe.totalNutrients.PROCNT.quantity)} ${recipe.recipe.totalNutrients.PROCNT.unit} 
+            <br> Carbs: ${Math.round(recipe.recipe.totalNutrients.CHOCDF.quantity)} ${recipe.recipe.totalNutrients.CHOCDF.unit}
+            <br> Fats: ${Math.round(recipe.recipe.totalNutrients.FAT.quantity)} ${recipe.recipe.totalNutrients.FAT.unit}`;
+            appendNode(li, title);
+            appendNode(li, img);
+            appendNode(li, nutrp);
+            appendNode(ul, li);
+        })
+    })
+};
