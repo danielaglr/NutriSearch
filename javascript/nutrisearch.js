fb687998b1215194e67222729a7b3adbd9f26a57
Daniel <81238732+danielaglr@users.noreply.github.com>
2022-01-05 10:14:36 -0800

    Got API search working
    Finalized rough draft of API search and began work on alerts for no results found

--- javascript/nutrisearch.js
+++ javascript/nutrisearch.js
@@ -16,26 +16,42 @@ async function apiSearch() {
 
     const edamamUrl = `https://api.edamam.com/search?q=${userInput}&app_id=${edamamId}&app_key=${edamamKey}`;
 
-    const ul = document.getElementsByClassName('recipe-display');
+    const ul = document.getElementById('recipe-display');
+    const resulthits = document.getElementById('hit-counter');
+
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+
+    if (resulthits.length != 0) {
+        resulthits.innerHTML = "";
+    }
 
     await fetch(edamamUrl).then(r => r.json()).then(function(data) {
         let recipes = data.hits
-        return recipes.map(function(recipe) {
-            let li = createNode('li');
-            let img = createNode('img');
-            let title = createNode('h3');
-            let nutrp = createNode('p');
-            img.src = recipe.recipe.image;
-            title.innerHTML = `${recipe.recipe.label}`;
-            nutrp.innerHTML = 
-            `Calories: ${Math.round(recipe.recipe.totalNutrients.ENERC_KCAL.quantity)} ${recipe.recipe.totalNutrients.ENERC_KCAL.unit} 
-            <br> Protein: ${Math.round(recipe.recipe.totalNutrients.PROCNT.quantity)} ${recipe.recipe.totalNutrients.PROCNT.unit} 
-            <br> Carbs: ${Math.round(recipe.recipe.totalNutrients.CHOCDF.quantity)} ${recipe.recipe.totalNutrients.CHOCDF.unit}
-            <br> Fats: ${Math.round(recipe.recipe.totalNutrients.FAT.quantity)} ${recipe.recipe.totalNutrients.FAT.unit}`;
-            appendNode(li, title);
-            appendNode(li, img);
-            appendNode(li, nutrp);
-            appendNode(ul, li);
-        })
+        console.log(edamamUrl);
+        console.log(recipes)
+        console.log(recipes.length)
+        if (recipes.length === 0) {
+            resulthits.innerHTML = "No results found."
+            } else {
+                return recipes.map(function(recipe) {
+                let li = createNode('li');
+                let img = createNode('img');
+                let title = createNode('h3');
+                let nutrp = createNode('p');
+                img.src = recipe.recipe.image;
+                title.innerHTML = `${recipe.recipe.label}`;
+                nutrp.innerHTML = 
+                    `Calories: ${Math.round(recipe.recipe.totalNutrients.ENERC_KCAL.quantity)} ${recipe.recipe.totalNutrients.ENERC_KCAL.unit} 
+                    <br> Protein: ${Math.round(recipe.recipe.totalNutrients.PROCNT.quantity)} ${recipe.recipe.totalNutrients.PROCNT.unit} 
+                    <br> Carbs: ${Math.round(recipe.recipe.totalNutrients.CHOCDF.quantity)} ${recipe.recipe.totalNutrients.CHOCDF.unit}
+                    <br> Fats: ${Math.round(recipe.recipe.totalNutrients.FAT.quantity)} ${recipe.recipe.totalNutrients.FAT.unit}`;
+                appendNode(li, title);
+                appendNode(li, img);
+                appendNode(li, nutrp);
+                appendNode(ul, li);
+            })
+        }
     })
 };
