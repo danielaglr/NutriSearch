d9c8f3192dbae8afac0b50c84c1295b51ec8c2ab
Ethan Dills <sapientioraet@gmail.com>
2022-01-07 10:12:58 -0800

    Able to fetch id now
    Able to get the id of the first result after fetching from the spoonacular api.

--- javascript/nutrition.js
+++ javascript/nutrition.js
@@ -9,14 +9,37 @@ searchAPI();
 }
 });
 
+/**
+    * - call API to get ID
+    * - use ID to get nutritional info about inputed food (ingredient search)
+    * - receive & display information
+    * - save results locally & modify (able to add macros of multiple ingredients)
+*/
+
 async function searchAPI(){
+    resultsNumber = 6; // number of results I want for each search
 
-    var user_input = document.getElementById("calorie-search-bar").value; // I don't know how to make a deep copy of raw_input, so I just call the method again
-    alert(user_input); // temporary way to let me know that searchAPI() is being called
+    var userInput = document.getElementById("calorie-search-bar").value; // I don't know how to make a deep copy of raw_input, so I just call the method again
+    // alert(user_input); // temporary way to let me know that searchAPI() is being called
     const SPOONACULAR_API_KEY = `9e94d130a25f49baa470a7148cef08fe`;
-    const SPOONACULAR_API = `https://api.spoonacular.com/food/ingredients/search?query=${user_input}&apiKey=${SPOONACULAR_API_KEY}&number=4`;
+    const SPOONACULAR_API_URL = `https://api.spoonacular.com/food/ingredients/search?query=${userInput}&apiKey=${SPOONACULAR_API_KEY}&number=${resultsNumber}`;
+    let idNumber;
+    
 
-    /**
-     * 
-     */
+    await fetch(SPOONACULAR_API_URL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            idNumber = data.results[0].id;
+            console.log(idNumber);
+            const SPOONACULAR_ID_URL = `https://api.spoonacular.com/food/ingredients/${idNumber}/information?amount=1`;
+            console.log(SPOONACULAR_ID_URL);
+
+            await fetch(SPOONACULAR_ID_URL)
+        })
+        .catch(error =>{
+            // handle error
+            alert("error found while fetching id for SPOONACULAR_API");
+ 
+        });  
 }
