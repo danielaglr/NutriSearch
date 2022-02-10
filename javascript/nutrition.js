a673f60b1f7c7a408c9a86245c40f01242728f23
Ethan Dills <sapientioraet@gmail.com>
2022-02-10 10:00:21 -0800

    Now able to fetch nutrients from multiple ingredients
    Now calls searchByID() multiple times, with a different ID number as the parameter each time, and logs nutrition for each food's macros in the console.

--- javascript/nutrition.js
+++ javascript/nutrition.js
@@ -1,4 +1,3 @@
-
 // if the return key is pressed in the search bar, run searchAPI()
 var raw_input = document.getElementById("calorie-search-bar");
 
@@ -8,36 +7,41 @@ event.preventDefault();
 findAPIID();
 }
 });
-
 /**
     * - call API to get ID
     * - use ID to get nutritional info about inputed food (ingredient search)
     * - receive & display information
     * - save results locally & modify (able to add macros of multiple ingredients)
 */
-
 async function findAPIID(){
     resultsNumber = 6; // number of results I want for each search
     var userInput = document.getElementById("calorie-search-bar").value; // I don't know how to make a deep copy of raw_input, so I just call the method again
-    // alert(user_input); // temporary way to let me know that searchAPI() is being called
     const SPOONACULAR_API_KEY = `9e94d130a25f49baa470a7148cef08fe`;
     const SPOONACULAR_API_URL = `https://api.spoonacular.com/food/ingredients/search?query=${userInput}&apiKey=${SPOONACULAR_API_KEY}&number=${resultsNumber}`;
     let idNumber;
     
-    // first searches the API for the ID of whatever inputed food, then searches the "search by ID" API for nutritional info about the inputed food
+    // first searches the API for the ID of whatever inputed food
     await fetch(SPOONACULAR_API_URL)
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            idNumber = data.results[0].id;
-            console.log('ID number is: ' + idNumber);
-            searchByID(idNumber);
+
+            /** saves ID numbers for each result from data in an array, 
+             *  and then runs searchByID() several times, each time 
+             *  with an element of that array (the ID number) as the parameter*/ 
+            let idNumbers = [];
+            for(k=0; k<resultsNumber; k++){
+                idNumbers.push(data.results[k].id);
+            }
+            for(j=0; j<idNumbers.length; j++){
+                searchByID(idNumbers[j]);
+            }
             
         })
         .catch(error =>{
             // handle error
             alert("error found while fetching id for SPOONACULAR_API");
- 
+            console.log(error);
         });  
 }
 
@@ -60,13 +64,20 @@ async function searchByID(foodIDNumber){
 
             let count = 0;
             let macroCount = 0;
-            let macroArray = ['Carbohydrates', 'Fat', 'Protein'];
-            nutritionInfo = idData.nutrition.nutrients; // represents the array in the json file which holds each macro/micro nutrient & its amount
-            while(count < nutritionInfo.length){
-                if( macroArray.includes(idData.nutrition.nutrients[count].name) ){
-                    console.log( 'Found macro: ' + idData.nutrition.nutrients[count].name);
+            let macroNames = ['Carbohydrates', 'Fat', 'Protein'];
+            let macroObjects = [];
+            while(count < idData.nutrition.nutrients.length){
+                if( macroNames.includes(idData.nutrition.nutrients[count].name) ){
+                    macroObjects.push(idData.nutrition.nutrients[count]);
+
                     macroCount++;
-                    if(macroCount >= 3){
+                    if( macroCount >= macroNames.length || count >= (idData.nutrition.nutrients.length - 1) ){
+
+                        console.log("Found macros: ");
+                        for(i=0; i<macroObjects.length; i++){
+                            console.log("- " + macroObjects[i].name + ": " + macroObjects[i].amount + " " + macroObjects[i].unit);
+                        }
+
                         break;
                     }
                     count++;
