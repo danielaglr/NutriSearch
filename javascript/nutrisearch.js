58e25b95b9fb4e13cb4dc0d9318b1729584e6837
Daniel <81238732+danielaglr@users.noreply.github.com>
2022-02-17 10:10:49 -0800

    Created save to local storage function
    Created skeleton function which will be used to save recipes to local storage in order to display them on website startup in the future

--- javascript/nutrisearch.js
+++ javascript/nutrisearch.js
@@ -28,14 +28,19 @@ async function apiSearch() {
 
     await fetch(edamamUrl).then(r => r.json()).then(function(data) { // Uses API URL established previously to get results based on user input, returns in json format, and plugs data into a function.
         let recipes = data.hits
+        console.log(recipes)
         if (recipes.length === 0) { // Checks if 0 results and if so, displays an error message.
             document.getElementById('nrf-alert-id').classList.toggle('inactive')
             } else {
                 return recipes.map(function(recipe) { // Creates a new array function based on results.
+
                 let li = createNode('li'); // Creates elements using previously established function.
                 let img = createNode('img');
                 let title = createNode('h3');
                 let nutrp = createNode('p');
+                let idInfo = createNode('p');
+                let saveButton = createNode('button');
+
                 img.src = recipe.recipe.image;
                 title.innerHTML = `${recipe.recipe.label}`;
                 nutrp.innerHTML = // Adds all nutritional information into body element of node using dot notation to navigate results returned in JSON format.
@@ -43,9 +48,26 @@ async function apiSearch() {
                     <br> Protein: ${Math.round(recipe.recipe.totalNutrients.PROCNT.quantity)} ${recipe.recipe.totalNutrients.PROCNT.unit} 
                     <br> Carbs: ${Math.round(recipe.recipe.totalNutrients.CHOCDF.quantity)} ${recipe.recipe.totalNutrients.CHOCDF.unit}
                     <br> Fats: ${Math.round(recipe.recipe.totalNutrients.FAT.quantity)} ${recipe.recipe.totalNutrients.FAT.unit}`;
+                var unsplitrecipeID = `${recipe.recipe.uri}`; // Recieves raw URI which contains recipe ID.
+                var recipeIdentifier = unsplitrecipeID.split('_').pop(); // Takes raw URI and splits apart the section previous to the ID, returning only the ID.
+                idInfo.innerHTML = `${recipeIdentifier}`;
+                saveButton.id = 'save-recipe-btn';
+                saveButton.innerHTML = 'SAVE';
+                saveButton.onclick = function recipeSave() {
+                    console.log('Recipe save ran!')
+                    var recipeInfo = [
+                        recipeName = `${recipe.recipe.label}`,
+                        recipeID = `${recipeIdentifier}`
+                    ];
+                    window.localStorage.setItem('recipe', JSON.stringify(recipeInfo));
+                    console.log(window.localStorage.getItem('recipe'));
+                };
+
                 appendNode(li, title); // Appends elements to parent elements (parent, child) using previously established function.
                 appendNode(li, img);
                 appendNode(li, nutrp);
+                appendNode(li, idInfo);
+                appendNode(li, saveButton);
                 appendNode(ul, li);
             })
         }
